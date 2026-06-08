c4b271b2f1a7eb38b5abf87b501f794c5c1aff69
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-09 00:41:30 +0500

    and some functionality redirect sign-up page into login page

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let sign_up = document.querySelector("button");
+
+sign_up.addEventListener("click", function (e) {
+    e.preventDefault();
+    localStorage.setItem("name", name.value);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
+    Swal.fire({
+        title: "Sign-up Successfully !",
+        icon: "success",
+        draggable: true
+    }).then(() => {
+        window.location.href = "./pages/login.html"
+    })
+})
